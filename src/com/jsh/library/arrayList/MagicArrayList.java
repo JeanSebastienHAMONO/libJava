abe76306aef0a537a5a6389a87713423670ff59f
JeanSebastienHAMONO <jeansebastien.hamono@gmail.com>
2023-06-20 22:48:27 +0200

    2023-06-20

--- src/com/jsh/library/arrayList/MagicArrayList.java
+++ src/com/jsh/library/arrayList/MagicArrayList.java
@@ -1,2 +1,63 @@
-package com.jsh.library.arrayList;public class MagicArrayList {
+package com.jsh.library.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final public class MagicArrayList<T> {
+
+    /**
+     * ArrayList containing the data
+     */
+    private ArrayList<T> dataList = new ArrayList<>();
+
+    /**
+     * ArryList containing the attribut name for sorting the data
+     */
+    private ArrayList<String> sortedAttributsList = new ArrayList<>();
+
+    /* ----------------------------------------------------- */
+
+    public MagicArrayList() {
+
+    }
+
+    /* ----------------------------------------------------- */
+
+
+    public void printGenericList(List l) {
+        int nbLignes = l.size();
+        System.out.println("----------------------------------");
+        for(int i=0; i < nbLignes; i++) {
+            System.out.println(i + " : " + l.get(i));
+        }
+        System.out.println("----------------------------------");
+    }
+
+    public void printprintAttributsList() {
+        this.printGenericList(this.getSortedAttributsList());
+    }
+
+    public void printDataList() {
+            this.printGenericList(this.getDataList());
+        }
+
+    /* ----------------------------------------------------- */
+
+    public ArrayList<T> getDataList() {
+        return this.dataList;
+    }
+
+
+    public void setDataList(ArrayList<T> pDataList) {
+        this.dataList = pDataList;
+    }
+
+    public ArrayList<String> getSortedAttributsList() {
+        return this.sortedAttributsList;
+    }
+
+    public void setSortedAttributsList(ArrayList<String> pSortedAttributsList) {
+        this.sortedAttributsList = pSortedAttributsList;
+    }
+
 }
--- tests/com/tests/jsh/Main.java
+++ tests/com/tests/jsh/Main.java
@@ -1,7 +1,21 @@
-package com.jsh.tests;
+package com.tests.jsh;
+
+import com.jsh.library.arrayList.MagicArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("DEBUT");
+        System.out.println("------------------------------------");
+
+        MagicArrayList<String> mal = new MagicArrayList<>();
+
+        mal.getSortedAttributsList().add("Premier");
+        mal.getSortedAttributsList().add("Deuxième");
+        mal.getSortedAttributsList().add("Troisième");
+
+        mal.printAttributsList();
+
+        System.out.println("------------------------------------");
+        System.out.println("FIN");
     }
 }
